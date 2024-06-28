3fe0b7bc2b90a1a29c2126fa678d42d4ad3e8b35
Rajankumarmaruya <rajanmaurya0102@gmail.com>
2024-06-28 17:00:34 +0530

    Made the image card component

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,10 +5,11 @@ const Navbar = () => {
   return (
     <nav className='flex flex-col sm:flex-row justify-between items-start sm:items-center px-5 py-5 fixed top-0 left-0 right-0 shadow-md gap-1 sm:gap-0 z-30'>
       <div className='flex justify-between items-center'>
+        <img src="/picprismlogo.png" alt="our logo" className='w-[60px]' />
         <Link to="/" className='font-bold text-3xl'>Pic Prism</Link>
       </div>
 
-      <ul className='flex gap-5 text-lg text-gray-400-400'>
+      <ul className='flex gap-5 text-lg text-gray-400 ml-5 sm:ml-0'>
         <Link to="/about" className='hover:text-black cursor-pointer sm:p-2'>About</Link>
         <Link to="/contact" className='hover:text-black cursor-pointer sm:p-2'>Contact</Link>
         <Link to="/login" className='hover:text-black cursor-pointer sm:p-2'>Login</Link>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import HeroSection from '../components/HeroSection'
+import PhotoGallery from '../components/PhotoGallery'
 
 const Home = () => {
   return (
    <div className='mt-32'>
    <HeroSection/>
+   <PhotoGallery/>
    </div>
   )
 }
