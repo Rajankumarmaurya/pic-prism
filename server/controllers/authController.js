80a55ac078c20203fb5aa2eeead91caaad8d882d
Rajankumarmaruya <rajanmaurya0102@gmail.com>
2024-07-03 10:16:35 +0530

    day7 signup completed

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,4 +1,27 @@
+const User = require("../models/User");
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
+// THIS IS THE SIGN UP LOGIC
 const signup = async(req,res)=>{
+    const {username , email , password, accountType} = req.body;
+
+    try {
+        let user = await User.findOne({username});
+        if(user){
+            return res.status(400).json({message:"User already exist"})
+        }
+
+            const securePassword = await bcrypt.hash(password , 10);
+
+            user = new User({username, email, password:securePassword, accountType});
+            await user.save();
+            return res.status(200).json({success:true , message:"User registration Success"})
+
+
+    } catch (error) {
+        return res.status(500).json({success: false, message:error.message})
+    }
 
 }
 
--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
-const PORT = 5000;
 const dotenv = require("dotenv");
+const PORT = 5000;
+const cors = require("cors");
 const connect = require("./config/connection");
-
 dotenv.config();
+
 connect();
+app.use(cors());
+app.use(express.json());
 const { readdirSync } = require("fs");
 
 app.get("/", (req, res) => {
